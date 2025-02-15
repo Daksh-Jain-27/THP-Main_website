b313fc5e65892ea0ad9f3cd7dd3c1b5f395a9a11
ARJUN <arjunbhardwaj1509@gmail.com>
2025-02-15 16:41:21 +0530

    second

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import './Navbar.css';
 import { Button } from './button';
+import img1 from '../images/img1.png'
 function Navbar() {
     const [click,setClick]=useState(false);
     const [button,setButton]=useState(true);
@@ -22,69 +23,60 @@ function Navbar() {
     window.addEventListener('resize',showButton);
   return (
    <>
-   <nav className="navbar">
-    <div className="navbar-container">
-        <Link to='/' className='navbar-logo' onClick={closeMobileMenu} >
-        THP
-         <i className='fab fa-type3'/>
-        </Link>
-         <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
-         </div>
-         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-           <li className='nav-item'>
-            <Link to='/' className='nav-links' onclick={closeMobileMenu}>
-            Home
-            </Link>
-                
-           </li>
-           <li className='nav-item'>
-            <Link to='/About' className='nav-links' onclick={closeMobileMenu}>
-            About
-            </Link>
-                
-           </li>
-           <li className='nav-item'>
-            <Link to='/Team' className='nav-links' onclick={closeMobileMenu}>
-            Team
-            </Link>
-                
-           </li>
-           <li className='nav-item'>
-            <Link to='/Timeline' className='nav-links' onclick={closeMobileMenu}>
-            Timeline
-            </Link>
-                
-           </li>
-           <li className='nav-item'>
-            <Link to='/competitions' className='nav-links' onclick={closeMobileMenu}>
-            Competitions
-            </Link>
-                
-           </li>
-           <li className='nav-item'>
-            <Link to='/Gallery' className='nav-links' onclick={closeMobileMenu}>
-            Gallery
-            </Link>
-                
-           </li>
-           <li className='nav-item'>
-            <Link to='/Sponsors' className='nav-links' onclick={closeMobileMenu}>
-            Sponsors
-            </Link>
-                
-           </li>
-           <li className='nav-item'>
-            <Link to='/Contact Us' className='nav-links' onclick={closeMobileMenu}>
-            Contact Us
-            </Link>
-                
-           </li>
-         </ul>
-         {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
-    </div>
-
-   </nav>
+    <nav className="navbar">
+        <div className="navbar-container">
+          <a href="#home" className="navbar-logo" onClick={closeMobileMenu}>
+            <img src={img1} alt="logo" className="logo" />
+            {/* <span className="logo-text">THP</span> */}
+          </a>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <a href="#home" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#about" className="nav-links" onClick={closeMobileMenu}>
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#team" className="nav-links" onClick={closeMobileMenu}>
+                Team
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#timeline" className="nav-links" onClick={closeMobileMenu}>
+                Timeline
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#competitions" className="nav-links" onClick={closeMobileMenu}>
+                Competitions
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#gallery" className="nav-links" onClick={closeMobileMenu}>
+                Gallery
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#sponsors" className="nav-links" onClick={closeMobileMenu}>
+                Sponsors
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-links" onClick={closeMobileMenu}>
+                Contact Us
+              </a>
+            </li>
+          </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+        </div>
+      </nav>
    </>
   )
 }
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,11 +1,41 @@
-import '../../App.css'
-import Herosection  from '../Herosection'
+import './home.css'
+// import Herosection  from '../Herosection'
 function Home(){
     return (
-        <>
-        <Herosection/>
+       
+        <div>
+      <section id="home" >
+        <h1>Welcome to Home</h1>
+      </section>
 
-        </>
+      <section id="about" >
+        <h1>About Us</h1>
+      </section>
+
+      <section id="team" >
+        <h1>Our Team</h1>
+      </section>
+
+      <section id="timeline" >
+        <h1>Timeline</h1>
+      </section>
+
+      <section id="competitions" >
+        <h1>Competitions</h1>
+      </section>
+
+      <section id="gallery" >
+        <h1>Gallery</h1>
+      </section>
+
+      <section id="sponsors" >
+        <h1>Sponsors</h1>
+      </section>
+
+      <section id="contact" >
+        <h1>Contact Us</h1>
+      </section>
+    </div>
 
     );
 }
