262e377a27137f820ab3f6b5bcbbf972ae1b619f
Daksh-Jain-27 <djiitian27@gmail.com>
2025-03-20 18:20:01 +0530

    final

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,6 +4,7 @@ import img1 from '../../images/img1.png';
 import img3 from '../../images/img3.webp'; // Add more images
 import img4 from '../../images/img4.webp';
 import mbf from '../../images/mbf.jpg';
+import Timeline from './timeline';
 import React, { useState, useEffect } from 'react';
 import { FaFacebookF, FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
@@ -14,7 +15,6 @@ const competitions = [
     achievement: '🥇 1st Place - Business Plan',
   },
 ];
-import Timeline from './timeline';
 
 function Home() {
   const [showButton, setShowButton] = useState(false);
